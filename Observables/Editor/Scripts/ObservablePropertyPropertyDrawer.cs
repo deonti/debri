54badb08176623c557760afb5341c852e862b2d9
deonti <deontich@ya.ru>
2025-11-17 14:41:23 +0300

    feat(Observables.Editor): improve ObservableProperty drawing

---
+++ Observables/Editor/Scripts/ObservablePropertyPropertyDrawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Debri.Observables.Editor
+{
+  [CustomPropertyDrawer(typeof(ObservableProperty<>))]
+  public class ObservablePropertyPropertyDrawer : PropertyDrawer
+  {
+    public override VisualElement CreatePropertyGUI(SerializedProperty property)
+    {
+      var root = new VisualElement();
+      root.Add(new PropertyField(property.FindPropertyRelative("_value"), property.displayName));
+      return root;
+    }
+  }
+}
