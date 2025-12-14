b16a7ee8da9f09747f5fda08a0034c78fbb75d7b
deonti <deontich@ya.ru>
2025-12-14 13:50:16 +0300

    feat(Pools): implement prewarming component

---
+++ Pools/Scripts/GlobalPoolsPrewarm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Debri.Pools
+{
+  [AddComponentMenu("Debri/Pools/Global Pools Prewarm")]
+  [DefaultExecutionOrder(-999)]
+  public class GlobalPoolsPrewarm : MonoBehaviour
+  {
+    [SerializeField] private ItemSettings[] _items;
+
+    private void Awake()
+    {
+      foreach (ItemSettings settings in _items)
+        GlobalPools.Get(settings.ItemPrototype).Prewarm(settings.Count);
+    }
+
+    [Serializable]
+    private class ItemSettings
+    {
+      public GameObject ItemPrototype;
+      public int Count;
+    }
+  }
+}
