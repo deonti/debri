f20e0e137d90b3f8154e4306e02fedaec9307f58
deonti <deontich@ya.ru>
2025-12-14 13:49:25 +0300

    feat(Pools): implement prewarming methods

---
+++ Pools/Scripts/IPrewarmable.cs
@@ -0,0 +1,11 @@
+﻿namespace Debri.Pools
+{
+  /// <summary>
+  /// An interface that allows to prewarm a pool.
+  /// </summary>
+  public interface IPrewarmable
+  {
+    /// <summary>Prewarms the pool.</summary>
+    void Prewarm(int count);
+  }
+}
