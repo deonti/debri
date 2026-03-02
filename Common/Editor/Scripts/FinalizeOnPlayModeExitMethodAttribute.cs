80b2f9fab8186973d7030e733f3e5329063b236e
deonti <deontich@ya.ru>
2026-03-02 17:05:41 +0300

    feat(Common.Editor): implement FinalizeOnPlayModeExitMethodAttribute

---
+++ Common/Editor/Scripts/FinalizeOnPlayModeExitMethodAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Debri.Common.Editor
+{
+  /// <summary>
+  /// Marks a method to be invoked when the editor is about to exit play mode.
+  /// </summary>
+  /// <seealso cref="UnityEditor.InitializeOnLoadMethodAttribute"/>
+  /// <seealso cref="UnityEngine.RuntimeInitializeOnLoadMethodAttribute"/>
+  [MeansImplicitUse]
+  [AttributeUsage(AttributeTargets.Method)]
+  public class FinalizeOnPlayModeExitMethodAttribute : Attribute
+  {
+    public readonly bool InvokeIfDomainReloadEnabled;
+
+    /// <param name="invokeIfDomainReloadEnabled">
+    /// If true, the method will be invoked even if the domain reload is enabled.
+    /// Otherwise, the method will be invoked only if the domain reload is disabled.
+    /// </param>
+    /// <seealso cref="UnityEditor.EnterPlayModeOptions.DisableDomainReload"/>
+    public FinalizeOnPlayModeExitMethodAttribute(bool invokeIfDomainReloadEnabled = false) =>
+      InvokeIfDomainReloadEnabled = invokeIfDomainReloadEnabled;
+  }
+}
---
+++ Common/Editor/Scripts/Internals/FinalizeOnPlayModeExitProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Debri.Common.Editor.Internals
+{
+  internal static class FinalizeOnPlayModeExitProcessor
+  {
+    private static Finalizer[] _finalizers;
+
+    [InitializeOnLoadMethod]
+    private static void Initialize()
+    {
+      _finalizers = TypeCache.GetMethodsWithAttribute<FinalizeOnPlayModeExitMethodAttribute>()
+        .Select(method => new Finalizer(method)).ToArray();
+      if (_finalizers.Length == 0) return;
+
+      EditorApplication.playModeStateChanged += HandlePlayModeStateChanged;
+    }
+
+    private static void HandlePlayModeStateChanged(PlayModeStateChange state)
+    {
+      if (state is not PlayModeStateChange.ExitingPlayMode) return;
+
+      bool isDomainReloadEnabled =
+        !(EditorSettings.enterPlayModeOptionsEnabled
+          && EditorSettings.enterPlayModeOptions.HasFlag(EnterPlayModeOptions.DisableDomainReload));
+
+      foreach (Finalizer finalizer in _finalizers)
+        finalizer.Invoke(isDomainReloadEnabled);
+    }
+
+    private class Finalizer
+    {
+      private readonly MethodInfo _method;
+      private readonly bool _invokeIfDomainReloadEnabled;
+
+      public Finalizer(MethodInfo method)
+      {
+        _method = method;
+        _invokeIfDomainReloadEnabled = method.GetCustomAttribute<FinalizeOnPlayModeExitMethodAttribute>().InvokeIfDomainReloadEnabled;
+      }
+
+      public void Invoke(bool isDomainReloadEnabled)
+      {
+        if (isDomainReloadEnabled && !_invokeIfDomainReloadEnabled) return;
+
+        try
+        {
+          _method.Invoke(null, null);
+        }
+        catch (Exception exception)
+        {
+          Debug.LogException(exception);
+        }
+      }
+    }
+  }
+}
