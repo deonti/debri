eb5c9affa6b26682dcca89e154068b866fe7718d
deonti <deontich@ya.ru>
2026-04-08 20:54:37 +0300

    feat(PlayerLoop): implement a simpler way to add subsystems to PlayerLoop

---
+++ PlayerLoop/Scripts/Internal/UnityPlayerLoopSystemUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityPlayerLoopSystem = UnityEngine.LowLevel.PlayerLoopSystem;
+
+namespace Debri.PlayerLoop.Internal
+{
+  internal static class UnityPlayerLoopSystemUtils
+  {
+    public static void AddSubsystemTo<TParentSystem>(this ref UnityPlayerLoopSystem rootSystem, UnityPlayerLoopSystem subsystem)
+    {
+      Type parentSystemType = typeof(TParentSystem);
+      if (!TryAddSubsystemTo(ref rootSystem, parentSystemType, subsystem))
+        throw new Exception($"System of type {parentSystemType.Name} not found");
+    }
+
+    public static bool TryRemoveSubsystemFrom<TParentSystem>(this ref UnityPlayerLoopSystem rootSystem, Type subsystemType) =>
+      TryRemoveSubsystemFrom(ref rootSystem, typeof(TParentSystem), subsystemType);
+
+    private static bool TryAddSubsystemTo(ref UnityPlayerLoopSystem currentSystem, Type parentSystemType, UnityPlayerLoopSystem subsystem)
+    {
+      if (currentSystem.type == parentSystemType)
+      {
+        if (currentSystem.subSystemList is null)
+          currentSystem.subSystemList = new UnityPlayerLoopSystem[1];
+        else
+          Array.Resize(ref currentSystem.subSystemList, currentSystem.subSystemList.Length + 1);
+
+        currentSystem.subSystemList[^1] = subsystem;
+        return true;
+      }
+
+      if (currentSystem.subSystemList is not null)
+        for (int index = 0; index < currentSystem.subSystemList.Length; index++)
+          if (TryAddSubsystemTo(ref currentSystem.subSystemList[index], parentSystemType, subsystem))
+            return true;
+
+      return false;
+    }
+
+    private static bool TryRemoveSubsystemFrom(ref UnityPlayerLoopSystem currentSystem, Type parentSystemType, Type subsystemType)
+    {
+      if (currentSystem.subSystemList is null)
+        return false;
+
+      if (currentSystem.type == parentSystemType)
+      {
+        int subsystemIndex = Array.FindIndex(currentSystem.subSystemList, system => system.type == subsystemType);
+        if (subsystemIndex > -1)
+        {
+          for (int index = subsystemIndex; index < currentSystem.subSystemList.Length - 1; index++)
+            currentSystem.subSystemList[index] = currentSystem.subSystemList[index + 1];
+
+          Array.Resize(ref currentSystem.subSystemList, currentSystem.subSystemList.Length - 1);
+          return true;
+        }
+      }
+
+      for (int index = 0; index < currentSystem.subSystemList.Length; index++)
+        if (TryRemoveSubsystemFrom(ref currentSystem.subSystemList[index], parentSystemType, subsystemType))
+          return true;
+
+      return false;
+    }
+  }
+}
--- PlayerLoop/Scripts/PlayerLoopSystemUtils.cs
+++ PlayerLoop/Scripts/PlayerLoopSystemUtils.cs
@@ -1,8 +1,12 @@
 ﻿using System;
-using UnityEngine.LowLevel;
+using Debri.PlayerLoop.Internal;
+using PlayerLoopSystem = UnityEngine.LowLevel.PlayerLoopSystem;
+using System.Runtime.CompilerServices;
 
-namespace Debri.PlayerLoop
+// ReSharper disable once CheckNamespace
+namespace Debri.Common
 {
+  [Obsolete("Use PlayerLoopManager.")]
   public static class PlayerLoopSystemUtils
   {
     /// <summary> Adds a subsystem to a parent system</summary>
@@ -17,12 +21,10 @@ namespace Debri.PlayerLoop
     ///    PlayerLoop.SetPlayerLoop(rootSystem);
     /// </code>
     /// </example>
-    public static void AddSubsystemTo<TParentSystem>(this ref PlayerLoopSystem rootSystem, PlayerLoopSystem subsystem)
-    {
-      Type parentSystemType = typeof(TParentSystem);
-      if (!TryAddSubsystemTo(ref rootSystem, parentSystemType, subsystem))
-        throw new Exception($"System of type {parentSystemType.Name} not found");
-    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [Obsolete("Use PlayerLoopManager.AddSubsystem.")]
+    public static void AddSubSystemTo<TParentSystem>(this ref PlayerLoopSystem rootSystem, PlayerLoopSystem subsystem) =>
+      rootSystem.AddSubsystemTo<TParentSystem>(subsystem);
 
     /// <summary>
     /// Removes a subsystem from a parent system if it exists
@@ -31,72 +33,8 @@ namespace Debri.PlayerLoop
     /// <param name="subsystemType">Subsystem to remove</param>
     /// <typeparam name="TParentSystem">Type of the parent system</typeparam>
     /// <returns>True if the subsystem was removed</returns>
-    public static bool TryRemoveSubsystemFrom<TParentSystem>(this ref PlayerLoopSystem rootSystem, Type subsystemType) =>
-      TryRemoveSubsystemFrom(ref rootSystem, typeof(TParentSystem), subsystemType);
-
-    private static bool TryAddSubsystemTo(ref PlayerLoopSystem currentSystem, Type parentSystemType, PlayerLoopSystem subsystem)
-    {
-      if (currentSystem.type == parentSystemType)
-      {
-        if (currentSystem.subSystemList is null)
-          currentSystem.subSystemList = new PlayerLoopSystem[1];
-        else
-          Array.Resize(ref currentSystem.subSystemList, currentSystem.subSystemList.Length + 1);
-
-        currentSystem.subSystemList[^1] = subsystem;
-        return true;
-      }
-
-      if (currentSystem.subSystemList is not null)
-        for (int index = 0; index < currentSystem.subSystemList.Length; index++)
-          if (TryAddSubsystemTo(ref currentSystem.subSystemList[index], parentSystemType, subsystem))
-            return true;
-
-      return false;
-    }
-
-    private static bool TryRemoveSubsystemFrom(ref PlayerLoopSystem currentSystem, Type parentSystemType, Type subsystemType)
-    {
-      if (currentSystem.subSystemList is null)
-        return false;
-
-      if (currentSystem.type == parentSystemType)
-      {
-        int subsystemIndex = Array.FindIndex(currentSystem.subSystemList, system => system.type == subsystemType);
-        if (subsystemIndex > -1)
-        {
-          for (int index = subsystemIndex; index < currentSystem.subSystemList.Length - 1; index++)
-            currentSystem.subSystemList[index] = currentSystem.subSystemList[index + 1];
-
-          Array.Resize(ref currentSystem.subSystemList, currentSystem.subSystemList.Length - 1);
-          return true;
-        }
-      }
-
-      for (int index = 0; index < currentSystem.subSystemList.Length; index++)
-        if (TryRemoveSubsystemFrom(ref currentSystem.subSystemList[index], parentSystemType, subsystemType))
-          return true;
-
-      return false;
-    }
-  }
-}
-
-namespace Debri.Common
-{
-  using System.Runtime.CompilerServices;
-  using PlayerLoop;
-
-  [Obsolete("Use Debri.PlayerLoop.PlayerLoopSystemUtils instead.")]
-  public static class PlayerLoopSystemUtils
-  {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [Obsolete("Use Debri.PlayerLoop.PlayerLoopSystemUtils.AddSubSystemTo instead.")]
-    public static void AddSubSystemTo<TParentSystem>(this ref PlayerLoopSystem rootSystem, PlayerLoopSystem subsystem) =>
-      rootSystem.AddSubsystemTo<TParentSystem>(subsystem);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [Obsolete("Use Debri.PlayerLoop.PlayerLoopSystemUtils.TryRemoveSubSystemFrom instead.")]
+    [Obsolete("See PlayerLoopManager.AddSubsystem.")]
     public static bool TryRemoveSubSystemFrom<TParentSystem>(this ref PlayerLoopSystem rootSystem, Type subsystemType) =>
       rootSystem.TryRemoveSubsystemFrom<TParentSystem>(subsystemType);
   }
